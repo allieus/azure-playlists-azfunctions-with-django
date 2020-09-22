74c31ffaf8c1964d701700bb0f53724361e44fc8
Chinseok Lee <me@askcompany.kr>
2020-09-22 16:09:40 +0900

    Completed Episode 3

---
+++ dj_proj/blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Post(models.Model):
+    message = models.TextField()
+    photo = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
